af45b9fa8782bced089fedb8cecd59bf6438d682
doloreshaz3 <claudia.holik.pardo@estudiante.epsum.school>
2026-09-24 01:04:28 +0200

    Migrate PerfumeDto to kotlinx.serialization

--- app/src/main/java/com/kaori/android/data/PerfumeDto.kt
+++ app/src/main/java/com/kaori/android/data/PerfumeDto.kt
@@ -4,7 +4,7 @@ import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
-data class PerfumeDto(
+data class gPerfumeDto(
     val id: Int,
     val name: String,
     val brand: String,
